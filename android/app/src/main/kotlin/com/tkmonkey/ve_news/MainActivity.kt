059d6d695544aabde796a365f3a1f531c0de5ea9
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-07-15 16:34:59 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/tkmonkey/ve_news/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tkmonkey.ve_news
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar Text'),
+      ),
+      body: Container(
+        child: Text('HomePage'),
+      ),
+    );
+  }
+}
