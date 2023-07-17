d4e4d02f3fabfb1dd119865e72156947e5f10db2
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-07-16 19:53:50 -0500

    assets for channels

--- lib/config/res/app_assets.dart
+++ lib/config/res/app_assets.dart
@@ -1,3 +1,26 @@
-abstract class AppAssets {
-  static const String logo = 'assets/logo.png';
+class AppAssets {
+  AppAssets._();
+
+  static final channels = _AssetsChannels._();
+}
+
+class _AssetsChannels {
+  _AssetsChannels._();
+
+  final bbcPNG = 'assets/channels/bbc.png';
+  final bloombergPNG = 'assets/channels/bloomberg.png';
+  final cnbcPNG = 'assets/channels/cnbc.png';
+  final cnnPNG = 'assets/channels/cnn.png';
+  final dailyMailPNG = 'assets/channels/daily_mail.png';
+  final forbesPNG = 'assets/channels/forbes.png';
+  final foxNewsPNG = 'assets/channels/fox_news.png';
+  final msnPNG = 'assets/channels/msn.png';
+  final newYorkPostPNG = 'assets/channels/new_york_post.png';
+  final news18PNG = 'assets/channels/news_18.png';
+  final theGuardianPNG = 'assets/channels/the_guardian.png';
+  final theNewYorkTimesPNG = 'assets/channels/the_new_york_times.png';
+  final theSunPNG = 'assets/channels/the_sun.png';
+  final usaTodayPNG = 'assets/channels/usa_today.png';
+  final washingtonPostPNG = 'assets/channels/washington_post.png';
+  final yahooNewsPNG = 'assets/channels/yahoo_news.png';
 }
