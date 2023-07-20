830fda272efeda80cd665c809eb2bcafcdb8e243
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-07-19 23:02:20 -0500

    create setup use case

--- lib/domain/app_setup/use_case/app_setup_use_case.dart
+++ lib/domain/app_setup/use_case/app_setup_use_case.dart
@@ -1,1 +1,18 @@
-final class AppSetupUseCase {}
+import 'package:ve_news/cross/domain/repository/preferences_repository.dart';
+import 'package:ve_news/domain/channel/repository/sources_repository.dart';
+
+final class AppSetupUseCase {
+  final PreferencesRepository _preferencesRepository;
+  final SourcesRepository _sourcesRepository;
+
+  AppSetupUseCase(this._preferencesRepository, this._sourcesRepository);
+
+  Future<void> call() async {
+    final isFirstTime = _preferencesRepository.isFirstTime;
+
+    if (isFirstTime) {
+      await _sourcesRepository.createDefaultSources();
+      await _preferencesRepository.updateFirstTime();
+    }
+  }
+}
