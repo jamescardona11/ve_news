6a7e73c61c62254b4eb55fe8a68954a1634e1d0b
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-07-24 22:50:45 -0500

    Squashing codemagic config
    Add base test
    
    Change projectile dependecy
    
    Change group name
    
    Add runner for codemagic
    
    Add environments vars
    
    temp env
    
    min sdk version
    
    fix issue with sign android
    
    push origin main

--- lib/config/environment/env.dart
+++ lib/config/environment/env.dart
@@ -5,7 +5,7 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract final class Env {
   @EnviedField(varName: 'OPEN_AI_KEY', obfuscate: true)
-  static String openAIKey = _Env.openAIKey;
+  static String openAIKey = '_Env.openAIKey';
 
   @EnviedField(varName: 'AUDIO_API', obfuscate: true)
   static String audioAPI = _Env.audioAPI;
