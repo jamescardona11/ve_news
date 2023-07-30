8bc219c379c5a3c805d2482ce8c5e8671de852ae
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-07-29 19:07:39 -0500

    Create resubale combine

--- lib/data/article/articles_repository_impl.dart
+++ lib/data/article/articles_repository_impl.dart
@@ -122,32 +122,27 @@ final class ArticlesRepositoryImpl extends ArticlesRepository {
     return CombineLatestStream.combine2(
       _articleStore.where().sortByDateTime().watch(fireImmediately: true),
       _sourcesRepository.watch(),
-      (List<ArticleDto> articles, List<SourceModel> sources) => articles
-          .map((article) {
-            final source = sources.firstWhereOrNull((source) => source.id == article.sourceId);
-            if (source == null) return null;
-            return article.toModel(source);
-          })
-          .whereNotNull()
-          .toList(),
+      combiner,
     );
   }
 
   @override
   Stream<List<ArticleModel>> watchBookmarks() {
     return CombineLatestStream.combine2(
-      _articleStore.where().sortByDateTime().watch(fireImmediately: true),
+      _articleStore.filter().bookmarkEqualTo(true).sortByDateTime().watch(fireImmediately: true),
       _sourcesRepository.watch(),
-      (List<ArticleDto> articles, List<SourceModel> sources) => articles
-          .map((article) {
-            final source = sources.firstWhereOrNull((source) => source.id == article.sourceId);
-            if (source == null) return null;
-            return article.toModel(source);
-          })
-          .whereNotNull()
-          .toList(),
+      combiner,
     );
   }
+
+  List<ArticleModel> combiner(List<ArticleDto> articles, List<SourceModel> sources) => articles
+      .map((article) {
+        final source = sources.firstWhereOrNull((source) => source.id == article.sourceId);
+        if (source == null) return null;
+        return article.toModel(source);
+      })
+      .whereNotNull()
+      .toList();
 
   static const _articlesKey = 'articles';
   static const _resultsKey = 'results';
